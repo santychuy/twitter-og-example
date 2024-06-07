dc6e18549c0ff0a15a27c66ec4168c29bfb93b78
Santiago Carrasco <jsanticc@hotmail.com>
2024-06-07 15:46:49 -0700

    feat: reorganize layouts and implement form to write tweet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import { redirect } from "next/navigation";
 
 import { createClient } from "./utils/supabase/server";
+import MainLayout from "./Main/layout";
+import AsideLayout from "./Aside/layout";
+import FormTweet from "./components/FormTweet";
 import Feed from "./components/Feed";
 import Loading from "./components/Feed/loading";
 import AuthButton from "./components/AuthButton/server";
@@ -10,18 +13,23 @@ export default async function Home() {
   const supabase = createClient();
 
   const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (session === null) redirect("/login");
+  if (user === null) redirect("/login");
 
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <AuthButton />
-
-      <Suspense fallback={<Loading />}>
-        <Feed />
-      </Suspense>
-    </main>
+    <div className="flex min-h-screen">
+      <AsideLayout>Left</AsideLayout>
+      <MainLayout>
+        <FormTweet avatarUrl={user.user_metadata.avatar_url} />
+        <Suspense fallback={<Loading />}>
+          <Feed />
+        </Suspense>
+      </MainLayout>
+      <AsideLayout className="flex justify-end">
+        <AuthButton />
+      </AsideLayout>
+    </div>
   );
 }
